b1c38f20e7ec8dca046a3c43d890b165573f9b23
Tina Quach <quacht@mit.edu>
2017-02-22 00:27:54 -0500

    program .5 done

--- tooschoolforcool.py
+++ tooschoolforcool.py
@@ -6,7 +6,9 @@
 # JavaScript version: http://nickm.com/poems/through_the_park.html
 
 import random, textwrap
-text = ["Everything slows",
+
+
+school = ["Everything slows",
 "The girl grasps for flimsy pages",
 "The girl sighs",
 "The girl strokes her chin",
@@ -18,23 +20,59 @@ text = ["Everything slows",
 "A tape recorder spins, tightly gripped",
 "It reminds the girl of her grandmother",
 "'Stop complaining!'",
-"'Turn those lemons into lemonade!'",
 "He encourages her",
 "'Keep going...'",
 "He urges her",
-"'No, those are melons...'",
+"'No'",
+"'Yes'",
 "They exchange a knowing glance",
 "The two circle the words",
-"Something weaves through the air",
+"Something sits in the air",
 "Her breathing quickens",
 "Locutions fails to form meaning",
 "The girl dashes, leaving words behind",
 "Thoughts scatter",
+"Understanding settles",
 "The girl races",
 "The man's there first",
-"Things are forgotten in carelessness",
-"She gapes"]
+"Things are forgotten in carelessness"]
+
+school = ["Everything slows",
+"The girl grasps for flimsy pages",
+"The girl sighs",
+"The girl strokes her chin",
+"The pages rustle",
+"The girl looks up at the clock",
+"The tutor paces the girl",
+"Locution after locution",
+"The tutor makes a fist behind his back",
+"A tape recorder spins, tightly gripped",
+"It reminds the girl of her grandmother",
+"'Stop complaining!'",
+"He encourages her",
+"'Keep going...'",
+"He urges her",
+"'No'",
+"'Yes'",
+"They exchange a knowing glance",
+"The two circle the words",
+"Something sits in the air",
+"Her breathing quickens",
+"Locutions fails to form meaning",
+"The girl dashes, leaving words behind",
+"Thoughts scatter",
+"Understanding settles",
+"The girl races",
+"The man's there first",
+"Things are forgotten in carelessness"]
+
+#source: https://www.reddit.com/r/OneParagraph/comments/5twii8/the_silence_is_deafening/
+# The Train
+reddit = [ "Sunset colors the sky.", "A train's horn blows in the distance", "A bike's bell dings next door", "A car screeches on the driveway", "I know that she is coming round", "I want to prepare a welcome", "Where will she take me next?" ,"The sound of her wheels turning", "","They churn and churn in my head", "I try to focus", "If only he came too.", "The train roars through", "How did it come to this?", "Who let that train get out of control?", "It's gone for now.", "But I know she'll be back again.", "The familiar smoke and the warmth of home.","Please stop this train.", "Sunset colors the sky.", "Darkness follows.", "Does the train sleep?" ]
+
+text = reddit
+
 while len(text) > 7:
     text.remove(random.choice(text))
-print "\nToo School for Cool\n\n" + \
-    textwrap.fill(". ... ".join(text) + ".", 80)
+print "\nThe Train\n"
+print textwrap.fill(". ... ".join(text) + ".", 80)
